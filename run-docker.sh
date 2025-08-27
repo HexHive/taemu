bec16ef1ec62a29e3a0e4182f85f955747250fd4
philipp <philipp.mao@epfl.ch>
2025-08-27 15:05:24 +0200

    fix shm

--- run-docker.sh
+++ run-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker run --rm --name emu --network host -it -v .:/srv -w /srv/emulator -v /dev/shm:/dev/shm --shm-size=10g ta_emu bash
+docker run --rm --name emu --network host -it -v .:/srv -w /srv/emulator -v /dev/shm:/dev/shm --ipc=host --shm-size=10g ta_emu bash
 
 # python3 -m emulate ../beanpod/
