47092fdfc452ebdbe27165fafb9d8d32c9b1a6f5
0ddc0de <marcel.busch@epfl.ch>
2025-09-04 15:41:39 +0200

    check for empty path in fuzz.sh

--- emulator/fuzz.sh
+++ emulator/fuzz.sh
@@ -16,18 +16,27 @@ if [ -z "$1" ]; then
     exit 0
 fi
 
-if [ -d "$1" ]; then 
-    harness="$1/harness.py"
-    ta=$(ls -1 "$1"/*.ta 2>/dev/null | head -n 1)
-    fuzz_in="$1/in"
-    fuzz_out="$1/out"
+in_path=`realpath $1`
+
+if [ -d "$in_path" ]; then
+    harness="$in_path/harness.py"
+    ta=$(ls -1 "$in_path"/*.ta 2>/dev/null | head -n 1)
+
+    fuzz_in="$in_path/in"
+    fuzz_out="$in_path/out"
+
+    if [ -z "$ta" ]; then
+        echo "Could not find TA in $in_path"
+        exit
+    fi
 else
-    ta="$1"
+    ta="$in_path"
     fuzz_in="/tmp/in"
     fuzz_out="tmp/out"
 fi
 
-ta_name="${ta::-3}" 
+
+ta_name="${ta::-3}"
 cp "$ta" rootfs/
 cp "${ta_name}.json" rootfs/
 
@@ -38,19 +47,21 @@ if [ -z "$2" ]; then
     if [ ! -e "$fuzz_in" ]; then
         mkdir $fuzz_in
         echo "foo" > "$fuzz_in/foo"
-    fi 
+    fi
 
-	if [ ! -e "$fuzz_out" ]; then
+    if [ ! -e "$fuzz_out" ]; then
         mkdir $fuzz_out
-    fi 
+    fi
 
-    if [ -d "$1" ]; then
+    if [ -d "$in_path" ]; then
         afl-fuzz -t 2000 -i $fuzz_in -o $fuzz_out -m none -U -- python3 -m emulate --fuzz @@ --fuzz_harness $harness "rootfs/$(basename "$ta")"
     else 
         afl-fuzz -t 2000 -i $fuzz_in -o $fuzz_out -m none -U -- python3 -m emulate --fuzz @@ "rootfs/$(basename "$ta")"
     fi
 else 
-    if [ -d "$1" ]; then
+    if [ -d "$in_path" ]; then
+        # swap these when you want to attach gdb to triage
+        #python3 -m emulate $3 --gdb --fuzz_replay $2 --fuzz_harness $harness "rootfs/$(basename "$ta")"
         python3 -m emulate $3 --fuzz_replay $2 --fuzz_harness $harness "rootfs/$(basename "$ta")"
     else
         python3 -m emulate $3 --fuzz_replay $2 "rootfs/$(basename "$v0")"
