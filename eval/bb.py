5b10af16092ff2e88093d534f54522b09ee60ae3
philipp <philipp.mao@epfl.ch>
2025-09-25 10:12:05 +0200

    whyyy

--- eval/bb.py
+++ eval/bb.py
@@ -181,6 +181,7 @@ def generate_graph_noorder(cfg, todo=None):
     print("nr gp apis", len([a for a in used_apis if a.api_type == "gp_api"]))
     print("nr libc apis", len([a for a in used_apis if a.api_type == "libc"]))
     print("nr tee apis", len([a for a in used_apis if a.api_type.startswith("tee")]))
+    print("max nodes: {max_nodes}")
     implemented_apis = []
     i = 0
     reachable.append(reachable_nodes(cfg, implemented_apis)) 
@@ -215,6 +216,7 @@ def generate_graph(cfg, todo=None):
     print("nr gp apis", len([a for a in used_apis if a.api_type == "gp_api"]))
     print("nr libc apis", len([a for a in used_apis if a.api_type == "libc"]))
     print("nr tee apis", len([a for a in used_apis if a.api_type.startswith("tee")]))
+    print("max nodes: {max_nodes}")
     implemented_apis = []
     i = 0
     reachable.append(reachable_nodes(cfg, implemented_apis))
