80983adcc9254e37015f6e0d0a01ce4487f88759
philipp <philipp.mao@epfl.ch>
2025-09-29 15:41:48 +0200

    .-.

--- eval/fuzz.py
+++ eval/fuzz.py
@@ -42,7 +42,7 @@ def main():
     for tee in tees:
         for harness in os.listdir(os.path.join(BASE, tee, "harness")):
             if not os.path.exists(os.path.join(BASE, tee, "harness", harness, "ta.txt")):
-                print(f'!!!!!! {os.path.join(BASE, tee, "harness", harness} has no ta.txt!!!!!')
+                print(f'!!!!!! {os.path.join(BASE, tee, "harness", harness)} has no ta.txt!!!!!')
                 exit(-1)
             ta_name = open(os.path.join(BASE, tee, "harness", harness, "ta.txt")).read()
             if not os.path.exists(os.path.join(BASE, tee, "harness", harness, ta_name)):
