ef402951c5fd7cba14eaeb7eb3734588b39791e1
spidermana <xuyiwen14@gmail.com>
2026-01-08 13:30:58 +0000

    chore: nothing important

--- swarm/src/main.rs
+++ swarm/src/main.rs
@@ -459,7 +459,7 @@ fn run_fuzz_jobs(
     );
             // add progress bar
     let pb = ProgressBar::new(all_jobs as u64);
-    pb.set_style(ProgressStyle::with_template("{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({eta})")
+    pb.set_style(ProgressStyle::with_template("\n{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({eta})\n")
         .unwrap()
         .with_key("eta", |state: &ProgressState, w: &mut dyn Write| write!(w, "{:.1}s", state.eta().as_secs_f64()).unwrap())
         .progress_chars("#>-"));
