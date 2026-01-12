0de60b47fb37d38d31774e9b5a00a4a6569b9c44
philipp <philipp.mao@epfl.ch>
2026-01-12 21:03:34 +0100

    feature: optee batch run script

--- swarm/batch-run-optee.sh
+++ swarm/batch-run-optee.sh
@@ -32,7 +32,7 @@ fi
 
 cd /root/TA_GP_emulator/swarm
 echo "1. Running original fuzzing..."
-echo "y" | cargo run -- -t .. -f /root/TA_GP_emulator/emulator/fuzz.sh -d $MINUTES_ORIGINAL_FUZZING > "org-fuzz-output$(date +day%d-%H_%M_%S).log" 2>&1
+echo "y" | cargo run -- -t .. -f /root/TA_GP_emulator/emulator/fuzz.sh -d $MINUTES_ORIGINAL_FUZZING -p optee > "org-fuzz-output$(date +day%d-%H_%M_%S).log" 2>&1
 
 echo "2. Deduplicating original fuzzing and generating suspicious_inputs_replay..."
 cd /root/TA_GP_emulator
@@ -42,7 +42,7 @@ python3 eval/annotate_fetches.py
 
 cd /root/TA_GP_emulator/swarm
 echo "3. Running df fuzzing..."
-echo "y" | cargo run -- -t .. -f /root/TA_GP_emulator/emulator/df_fuzz.sh -d $MINUTES_DF_FUZZING -s > "df-fuzz-output$(date +day%d-%H_%M_%S).log" 2>&1
+echo "y" | cargo run -- -t .. -f /root/TA_GP_emulator/emulator/df_fuzz.sh -d $MINUTES_DF_FUZZING -s -p optee > "df-fuzz-output$(date +day%d-%H_%M_%S).log" 2>&1
 
 end_time=$(date +%s)
 duration=$((end_time - start_time))
