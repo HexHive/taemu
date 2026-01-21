f76632f3ede8ecccd36966153ca501b82504732e
philippmao <maophilipp@gmail.com>
2026-01-21 18:42:27 +0100

    analyze numbers

--- eval/reshaping_cmp.py
+++ eval/reshaping_cmp.py
@@ -25,9 +25,11 @@ ignore_harness = "86f6_fuzz"
 q = queue.Queue() 
 
 class Job:
-    def __init__(self, harness_path, duration):
+    def __init__(self, harness_path, duration, df_out, sus_out=None):
         self.harness_path = harness_path
         self.duration = duration
+        self.df_out_path = df_out
+        self.sus_out_path = sus_out
 
 def to_emu_name(i):
     return f'emu_{i}'
@@ -75,7 +77,7 @@ def gather_jobs(args):
                 if ignore_harness in harness_path: continue
                 num_dfs = len(os.listdir(root))
                 print(f'[^] {harness_path} num dfs: {num_dfs}', flush=True)
-                q.put(Job(harness_path, num_dfs * args.df_fuzz_time))
+                q.put(Job(harness_path, num_dfs * args.df_fuzz_time, root))
     else:
         path = os.path.join(args.fuzz_backup, 'df_fuzz')
         for harness in os.listdir(path):
@@ -84,7 +86,7 @@ def gather_jobs(args):
             if ignore_harness in harness_path: continue
             num_dfs = len(os.listdir(os.path.join(path, harness, 'df_fuzz')))
             print(f'[^] {harness_path} num dfs: {num_dfs} {num_dfs * args.df_fuzz_time}', flush=True)
-            q.put(Job(harness_path, num_dfs * args.df_fuzz_time)) 
+            q.put(Job(harness_path, num_dfs * args.df_fuzz_time, os.paht.join(path, harness, 'df_fuzz'))) 
 
 def validate(args):
     if os.path.exists("/.dockerenv"):
@@ -121,6 +123,41 @@ def validate(args):
     print("[+] Emulator containers started")
 
 
+def analyze_task(job):
+    fuzzed_sus2reghash = {} 
+    fuzzed_controlflowhashes = []
+    dff_execs = 0
+    dff_execs_2 = 0
+    done = []
+    for df_out in os.listdir(job.df_out_path):
+        df_seed, reg_hash = df_out.split("/")
+        dff_execs += get_execs(os.path.join(df_out, 'out', 'default')) 
+        if df_seed not in done:
+            done.append(df_seed)
+            dff_execs_2 += get_execs(os.path.join(df_out, 'out', 'default'))
+    print(f'{job.harness} {dff_execs} {dff_execs_2}')
+    rsh_execs = 0
+    rsh_execs_df = 0
+    for out in os.listdir(os.path.join(job.harness_path, 'out')):
+        rsh_execs += get_execs(os.path.join(job.harness_path, 'out', out))
+    for jsson in os.listdir(os.path.join(job.harness_path, 'record_meta')):
+        if jsson.endswith('hash2count.json'):
+            a = json.load(os.path.join(job.harness_path, 'record_meta', jsson))
+            for k,v in a.items():
+                rsh_execs_df  += v
+    print(f'{job.harness} {rsh_execs} {rsh_execs_df}')
+
+def print_numbers(args):
+    gather_jobs(args)  
+    while True:
+        try:
+            job = q.get_nowait()
+        except queue.Empty:
+            break
+        analyze_task(job)
+        q.task_done()
+     
+
 if __name__ == "__main__":
     os.system(f'docker rm -f $(docker ps -aq)')
 
@@ -129,12 +166,17 @@ if __name__ == "__main__":
     parser.add_argument("--fuzz-backup", type=str, required=False, help="if set will use fuzzing backup data from this path")
     parser.add_argument("--df-fuzz-time", type=int, default=15*60, required=False, help="time for df-fuzzing")
     parser.add_argument("--num-containers", type=int, default=5)
+    parser.add_argument("--print-numbers", type=bool, action="store_true", default=False)
     
     args = parser.parse_args()
     
     if "eval" in os.getcwd() or "TA_GP_emulator" not in os.getcwd():
         print(f"[-] Please run deduplicate.py at /{os.getlogin()}/TA_GP_emulator")
         exit(1)
+
+    if args.print_numbers:
+        print_numbers(args)
+        exit(0)
     
     validate(args)
 
