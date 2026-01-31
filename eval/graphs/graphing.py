e3d031c8131e1403a74bb1a6861031edfbbc893c
philipp <philipp.mao@epfl.ch>
2026-01-31 12:01:40 +0100

    ??

--- eval/graphs/graphing.py
+++ eval/graphs/graphing.py
@@ -363,11 +363,8 @@ def gather_suspicious_inputs_covs(df_bar_key: str, df_group_finfo: GroupedFuzzin
     # align with x axis, so should be id or harness_path
     suspicious_inputs_bbs = list()
     harness_path = df_group_finfo.raw_fuzzing_info.harness_path
-    suspicious_inputs_covs = os.path.join(bk_suspicious_inputs_cov_rdir, 
-        harness_path[harness_path.rfind("TA_GP_emulator/")+len("TA_GP_emulator/"):], 
-        "out", 
-        "cov"
-    )
+    harness_name = harness_path[harness_path.rfind("TA_GP_emulator/")+len("TA_GP_emulator/"):]
+    suspicious_inputs_covs = f'{bk_suspicious_inputs_cov_rdir}/{harness_name}/out/cov' 
     print("bk sus dir???", bk_suspicious_inputs_cov_rdir, suspicious_inputs_covs)
     if bar_field_name == "id":
         file = df_bar_key.split("_")[-2] + ".cov"
