97846aa7aedf0498b652a5ff9c7a81bf7c945c3d
philipp <philipp.mao@epfl.ch>
2026-01-31 14:56:10 +0100

    pls

--- eval/graphs/graphing.py
+++ eval/graphs/graphing.py
@@ -575,12 +575,46 @@ def df_control_flow_graph(
             [basic_segments, part_one_segments, part_two_segments, part_three_segments]
         )"""
 
+        segments = [
+            ("Basic", basic_segments),
+            ("Part 1", part_one_segments),
+            ("Part 2", part_two_segments),
+            ("Part 3", part_three_segments),
+        ]
+
+        x = np.arange(len(basic_segments))
+
+        # Prepare containers for plotting
+        stacked_values = {name: [] for name, _ in segments}
+        stacked_bottoms = {name: [] for name, _ in segments}
+
+        for i in range(len(x)):
+            # collect values for this bar
+            values = [(name, arr[i]) for name, arr in segments]
+
+            # sort by size (ascending; use reverse=True for largest at bottom)
+            values.sort(key=lambda v: v[1])
+
+            bottom = 0
+            for name, val in values:
+                stacked_values[name].append(val)
+                stacked_bottoms[name].append(bottom)
+                bottom += val
+
         x = np.arange(len(basic_segments))  # bar positions
 
-        ax.bar(x, basic_segments, label="Basic")
-        ax.bar(x, part_one_segments, bottom=basic_segments, label="Part 1")
-        ax.bar(x, part_two_segments, bottom=np.array(basic_segments)+np.array(part_one_segments), label="Part 2")
-        ax.bar(x, part_three_segments, bottom=np.array(basic_segments)+np.array(part_one_segments)+np.array(part_two_segments), label="Part 3")
+        for name in stacked_values:
+            ax.bar(
+                x,
+                stacked_values[name],
+                bottom=stacked_bottoms[name],
+                label=name,
+            )
+
+        #ax.bar(x, basic_segments, label="Basic")
+        #ax.bar(x, part_one_segments, bottom=basic_segments, label="Part 1")
+        #ax.bar(x, part_two_segments, bottom=np.array(basic_segments)+np.array(part_one_segments), label="Part 2")
+        #ax.bar(x, part_three_segments, bottom=np.array(basic_segments)+np.array(part_one_segments)+np.array(part_two_segments), label="Part 3")
         """
         ax.plot(
             list(range(0, len(basic_segments))),
