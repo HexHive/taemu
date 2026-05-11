4a96bdb0cb5c0f4f9ff22f24037902a1e347a58f
Aljaž Medič <35614267+aljazmedic@users.noreply.github.com>
2026-05-11 21:57:32 +0200

    2 harnesses

--- qsee_nongp/harness/fuzz_soter64.nopauth/harness.py
+++ qsee_nongp/harness/fuzz_soter64.nopauth/harness.py
@@ -1,27 +1,3 @@
-#!/bin/bash
-
-set -e
-
-[ -z "$1" ] && echo "usage: $0 <path to ta>" && exit 1
-
-ta_path=$1
-[ ! -f "$ta_path" ] && echo "TA not found" && exit 1
-ta_name=$(basename "$ta_path")
-
-#Adjacent
-yml_path=${ta_path%.*}.yml
-echo "Using TA YML: $yml_path"
-[ ! -f "$yml_path" ] && echo "TA YML not found" && exit 1
-
-echo "Using TA: $ta_name"
-script_dir=$(dirname "$0")
-harness_dir=$(realpath ${script_dir}/fuzz_${ta_name%.*})
-echo "Harness directory: $harness_dir"
-
-mkdir -p ${harness_dir}/{in,out}
-ln -sfr -t "$harness_dir" "$ta_path" "$yml_path"
-
-cat <<HARNESS_EXAMPLE >${harness_dir}/harness.py
 from typing import TYPE_CHECKING
 import pwn
 from qiling import Qiling
@@ -46,7 +22,7 @@ def place_input_callback(ql: Qiling, input: bytes, iters: int):
     if len(input) < 8:
         return False
 
-    cmds = [1,2,0xc001]
+    cmds = [257, 258, 259, 260, 261, 262, 263, 264, 265, 266, 267, 268, 269, 270, 271]
     cmd = cmds[input[0] % len(cmds)]
 
     data = pwn.flat({0: pwn.p32(cmd), 4: input[1:]})
@@ -58,4 +34,3 @@ def place_input_callback(ql: Qiling, input: bytes, iters: int):
 
     setup_qsee_fuzz(ql, cmd_params, input)
     return True
-HARNESS_EXAMPLE
