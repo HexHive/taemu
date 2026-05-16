6cbd48eadc183b7139da31a775d7fc4b935cdd52
Aljaž Medič <35614267+aljazmedic@users.noreply.github.com>
2026-05-16 03:05:04 +0200

    Add vaultkeeper harness, notimplemented completely

---
+++ qsee_nongp/harness/fuzz_vaultkeeper.nopauth/harness.py
@@ -0,0 +1,37 @@
+from typing import TYPE_CHECKING
+import pwn
+from qiling import Qiling
+from pathlib import Path
+
+
+if TYPE_CHECKING:
+    from emulator.emulate.non_gp.qsee.params import QseeCommandParams, setup_qsee_fuzz
+else:
+    from emulate.non_gp.qsee.params import QseeCommandParams, setup_qsee_fuzz
+
+filename = Path(__file__).stem.replace("_fuzz", "")
+
+
+def place_input_callback(ql: Qiling, input: bytes, iters: int):
+    del iters  # We are not using pers iters
+    ql.log.info("%s custom harness!!!! Placing input: %s", filename.upper(), input[:10])
+
+    # Example setup
+    REQ_LEN = 0xadf8
+    RSP_LEN = 0xae00
+    if len(input) < 8:
+        return False
+
+    cmds = [1,2,0xc001]
+    cmd = cmds[input[0] % len(cmds)]
+
+    # if first 4 bytes == -0x3f21fffd, it is from bootloader?
+    data = pwn.flat({0: pwn.p32(cmd), 4: input[1:]})
+
+    data = data[:REQ_LEN]
+    data = data + (REQ_LEN - len(data)) * b"\x00"
+
+    cmd_params = QseeCommandParams(data, req_len=REQ_LEN, rsp_len=RSP_LEN)
+
+    setup_qsee_fuzz(ql, cmd_params, input)
+    return True
