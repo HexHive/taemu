b9db4ce0d2d69709a454dd6246efe8090fcade48
Aljaž Medič <35614267+aljazmedic@users.noreply.github.com>
2026-05-20 21:58:29 +0200

    Add nonfaithful decorator and retriever at the end.

--- emulator/emulate/non_gp/qsee/api_common.py
+++ emulator/emulate/non_gp/qsee/api_common.py
@@ -1,6 +1,12 @@
+from collections import defaultdict
 import functools
+from typing import TYPE_CHECKING
 from qiling import Qiling
 import struct
+
+if TYPE_CHECKING:
+    from emulate.non_gp.qsee.models import HookData
+    from emulate.ta_mgr import TAEMU
 
 
 def _wrap_fcall_with_debug_log(func):
@@ -26,3 +32,20 @@ def _log_args(ql: Qiling, name: str, args: dict):
         ", ".join(f"{k}={v:#x}" if isinstance(v, int) else f"{k}={v}" for k, v in args.items()),
         ql.arch.regs.lr,
     )
+
+def nonfaithful(func):
+    @functools.wraps(func)
+    def wrapper(ql: Qiling, hook_data: 'HookData'):
+        arm_instruction_size = 0x4
+        ql.log.warning("Non-faithful call to %s from %#x", hook_data.func_name, ql.arch.regs.lr - arm_instruction_size)
+        d = getattr(hook_data.emu, '__nonfaithful_frequency', None)
+        if d is None:
+            d = hook_data.emu.__nonfaithful_frequency = defaultdict(int)
+        d[hook_data.func_name] += 1
+        return func(ql, hook_data)
+    return wrapper
+
+def emu_report_nonfaithful(emu: 'TAEMU'):
+    d = getattr(emu, '__nonfaithful_frequency', defaultdict(int))
+    for func_name, count in d.items():
+        emu.ql.log.warning("Non-faithful call to %s: %d times", func_name, count)
